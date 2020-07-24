869201c4eb699d669d211a7de7da4e41e54d53b6
Mehmet Guler <mehmetgspace@gmail.com>
2020-07-24 15:42:32 +0300

    Add grunt-contrib-sass plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,20 @@ module.exports = function(grunt) {
 
   // Project configuration
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          './app/assets/styles/styles.css': './app/assets/styles/styles.scss'
+        }
+      }
+    }
   });
 
   // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Default tasks
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['sass']);
 
 }
