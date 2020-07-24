9c49475ab1f28c2e08855bf1189f3a73136edece
Mehmet Guler <mehmetgspace@gmail.com>
2020-07-24 14:49:22 +0300

    Add grunt package

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  // Load the plugins
+
+  // Default tasks
+  grunt.registerTask('default', []);
+
+}
